67270ed30b3f4f76fb9ab958b00bd44cb3f39253
Kwabena Fuachie <kafuachie11@gmail.com>
2025-12-02 14:24:48 +0000

    :sparkles: add solution for day 2 part 2

--- solutions/day_2/main.go
+++ solutions/day_2/main.go
@@ -45,6 +45,45 @@ func getNumsWithTwiceRepeatedDigitsSequenceWithinRange(rangeVal []int) []int {
   return nums
 }
 
+func getNumsWithRepeatedDigitsSequenceWithinRange(rangeVal []int) []int {
+  // Brute force
+  var nums []int
+  for i := rangeVal[0]; i <= rangeVal[1]; i++ {
+    numStr := strconv.Itoa(i)
+
+    isRepeating := false
+    IntervalLoop:
+    for j := 1; j <= len(numStr)/2; j++ {
+      if j != 1 && len(numStr) % j != 0 {
+        continue
+      }
+
+      intervalDigits,isRepeatingForInterval := numStr[0:j], false
+      for k := 0; k < len(numStr); k+=j {
+        if numStr[k:k+j] == intervalDigits  {
+          isRepeatingForInterval = true
+          continue
+        }
+
+        isRepeatingForInterval = false
+        break
+      }
+
+      if isRepeatingForInterval {
+        isRepeating = true
+        break IntervalLoop
+      }
+    }
+
+    if !isRepeating {
+      continue
+    }
+
+    nums = append(nums, i)
+  }
+  return nums
+}
+
 func part1(rangeVals [][]int) string {
   totalInvalidIdsSum := 0
   for _, rangeVal := range rangeVals {
@@ -56,7 +95,13 @@ func part1(rangeVals [][]int) string {
 }
 
 func part2(rangeVals [][]int) string {
-  return ""
+  totalInvalidIdsSum := 0
+  for _, rangeVal := range rangeVals {
+    for _, invalidId := range getNumsWithRepeatedDigitsSequenceWithinRange(rangeVal) {
+      totalInvalidIdsSum += invalidId
+    }
+  }
+  return strconv.Itoa(totalInvalidIdsSum)
 }
 
 func main() {
