ee553ef5de08e391662c5bdfcc197ea78bc2a9b6
Kwabena Fuachie <kafuachie11@gmail.com>
2025-12-05 13:09:53 +0000

    :sparkles: add solutions for day 5

---
+++ solutions/day_5/main.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/ka-fuachie/advent-of-code-2025/util"
+)
+
+func min(a int, b int) int {
+  if a < b {
+    return a
+  }
+  return b
+}
+
+func max(a int, b int) int {
+  if a > b {
+    return a
+  }
+  return b
+}
+
+type idRange struct {
+  start int
+  end int
+}
+
+func (self idRange) includes(id int) bool {
+  return self.start <= id && self.end >= id
+}
+
+func (self idRange) intersects(otherIdRange idRange) bool {
+  return (
+    self.includes(otherIdRange.start) ||
+    self.includes(otherIdRange.end) ||
+    otherIdRange.includes(self.start) ||
+    otherIdRange.includes(self.end))
+}
+
+func (self idRange) merge(otherIdRange idRange) (idRange, error) {
+  if !self.intersects(otherIdRange) {
+    return idRange{}, errors.New(
+      "Cannot merge no intersecting id ranges: " +
+      self.toString() + "," + otherIdRange.toString())
+  }
+
+  return idRange{
+    start: min(self.start, otherIdRange.start),
+    end: max(self.end, otherIdRange.end),
+  }, nil
+}
+
+func (self idRange) toString() string {
+  return strconv.Itoa(self.start) + "-" + strconv.Itoa(self.end)
+}
+
+func (self idRange) length() int {
+  return self.end - self.start + 1
+}
+
+func parseIdRange(idRangeStr string) (idRange, error) {
+  rangeStrList := strings.Split(idRangeStr, "-")
+
+  start, err := strconv.Atoi(rangeStrList[0])
+  if err != nil {
+    return idRange{}, err
+  }
+  end, err := strconv.Atoi(rangeStrList[1])
+  if err != nil {
+    return idRange{}, err
+  }
+
+  return idRange{start: start, end: end}, nil
+}
+
+func _resolveIdRanges(idRanges []idRange) []idRange {
+  partiallyResolvedIdRanges := make([]idRange, 0, len(idRanges))
+
+  for _, idRangeItem := range idRanges {
+    hasRangeIntersects := false
+    for resolvedIdRangeIndex, resolvedIdRange := range partiallyResolvedIdRanges {
+      mergedIdRanges, err := resolvedIdRange.merge(idRangeItem)
+      if err != nil {
+        continue
+      }
+
+      partiallyResolvedIdRanges[resolvedIdRangeIndex] = mergedIdRanges
+      hasRangeIntersects = true
+    }
+
+    if !hasRangeIntersects {
+      partiallyResolvedIdRanges = append(partiallyResolvedIdRanges, idRangeItem)
+    }
+  }
+
+  return partiallyResolvedIdRanges
+}
+
+func resolveIdRanges(idRanges []idRange) []idRange {
+  // brute force
+  resolveIdRanges := _resolveIdRanges(idRanges)
+  for {
+    partiallyResolveIdRanges := _resolveIdRanges(resolveIdRanges)
+    if len(partiallyResolveIdRanges) == len(resolveIdRanges) {
+      break
+    }
+
+    resolveIdRanges = partiallyResolveIdRanges
+  }
+
+  return resolveIdRanges
+}
+
+func part1(freshIdRanges []idRange, availableIds []int) string {
+  totalFreshAvailableingredients := 0
+  for _, id := range availableIds {
+    for _, idRange := range freshIdRanges {
+      if !idRange.includes(id) {
+        continue
+      }
+
+      totalFreshAvailableingredients+=1
+      break
+    }
+  }
+
+  return strconv.Itoa(totalFreshAvailableingredients)
+}
+
+func part2(freshIdRanges []idRange, _ []int) string {
+  totalIdsWithinFreshRanges := 0
+  for _, idRange := range resolveIdRanges(freshIdRanges) {
+    totalIdsWithinFreshRanges += idRange.length()
+  }
+
+  return strconv.Itoa(totalIdsWithinFreshRanges)
+}
+
+func main() {
+  input, err := util.ReadInput(5)
+  if err != nil {
+    panic(err)
+  }
+//   input := `3-5
+// 10-14
+// 16-20
+// 12-18
+//
+// 1
+// 5
+// 8
+// 11
+// 17
+// 32`
+
+  inputParts := strings.Split(input, "\n\n")
+  freshIdRangesPart := inputParts[0]
+  availableIdsPart := inputParts[1]
+
+  freshIdRangeStrList := strings.Split(strings.Trim(freshIdRangesPart, "\n"), "\n")
+  availableIdStrList := strings.Split(strings.Trim(availableIdsPart, "\n"), "\n")
+
+  freshIdRanges := make([]idRange, 0, len(freshIdRangeStrList))
+  for _, idRangeStr := range freshIdRangeStrList {
+    idRange, err := parseIdRange(idRangeStr)
+    if err != nil {
+      panic(err)
+    }
+    freshIdRanges = append(freshIdRanges, idRange)
+  }
+
+  availableIds := make([]int, 0, len(availableIdStrList))
+  for _, idStr := range availableIdStrList {
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+      panic(err)
+    }
+    availableIds = append(availableIds, id)
+  }
+
+  fmt.Println(part1(freshIdRanges, availableIds))
+  fmt.Println(part2(freshIdRanges, availableIds))
+}
